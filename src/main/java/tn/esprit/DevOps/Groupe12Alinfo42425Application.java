b7a52ba0c03a2606700a2cd8dccb1f719f701677
Anas Ben Talouba <anas.bentalouba@esprit.tn>
2025-04-10 00:18:13 +0100

    Initial DevOps project setup

---
+++ src/main/java/tn/esprit/DevOps/Groupe12Alinfo42425Application.java
@@ -0,0 +1,14 @@
+package tn.esprit.DevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Groupe12Alinfo42425Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Groupe12Alinfo42425Application.class, args);
+        System.out.println("test");
+    }
+
+}
---
+++ src/test/java/tn/esprit/DevOps/Groupe12Alinfo42425ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.DevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Groupe12Alinfo42425ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
