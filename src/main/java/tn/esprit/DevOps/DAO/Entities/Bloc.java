1af9c0869e45015ff00cffd5316d147de4a6fcc0
tayssir519 <emlaktayssir55@gmail.com>
2025-04-10 22:38:07 +0100

    Add Bloc.java entity

--- src/main/java/tn/esprit/DevOps/DAO/Entities/Bloc.java
+++ src/main/java/tn/esprit/DevOps/DAO/Entities/Bloc.java
@@ -5,10 +5,12 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+
+
+
 
 @Entity
 @Table(name = "T_BLOC")
@@ -19,5 +21,17 @@ import java.util.List;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Bloc implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long idBloc;
+    String nomBloc;
+    long capaciteBloc;
+
+    @ManyToOne
+    @JsonIgnore
+    Foyer foyer;
+    @OneToMany(mappedBy = "bloc", fetch = FetchType.EAGER)
+    @JsonIgnore
+    List<Chambre> chambres= new ArrayList<>();
    
 }
