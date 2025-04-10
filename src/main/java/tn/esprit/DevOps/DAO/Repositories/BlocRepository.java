b98052c4511548d3076b14c50d1dc883c5a8e7ce
ChaimaRazgui <shaimarazgui@gmail.com>
2025-04-10 15:24:16 +0100

    Foyer

---
+++ src/main/java/tn/esprit/DevOps/DAO/Repositories/BlocRepository.java
@@ -0,0 +1,7 @@
+package tn.esprit.DevOps.DAO.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tn.esprit.DevOps.DAO.Entities.Bloc;
+
+public interface BlocRepository extends JpaRepository<Bloc, Long> {
+}
