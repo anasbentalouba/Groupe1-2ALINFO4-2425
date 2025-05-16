139b789ad2307ec6f4138c895994638a16df092b
Tayssiremlak <emlaktayssir55@gmail.com>
2025-05-16 15:06:48 +0100

    update: fixed pom.xml to enable JUnit 5 tests

--- src/test/java/tn/esprit/devops/BlocServiceTest.java
+++ src/test/java/tn/esprit/devops/BlocServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.devops.dao.entities.Bloc;
 import tn.esprit.devops.dao.entities.Chambre;
 import tn.esprit.devops.dao.repositories.BlocRepository;
@@ -23,8 +22,7 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@SpringBootTest
-public class BlocServicetest {
+ public class BlocServiceTest {
     @Mock
     BlocRepository blocRepository;
     @Mock
