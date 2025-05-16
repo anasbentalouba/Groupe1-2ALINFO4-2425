037be0176c3977822f6677126aca31eed5dc2ec6
Anas Ben Talouba <99347338+anasbentalouba@users.noreply.github.com>
2025-05-16 15:13:22 +0100

    Update ChambreServiceImplTest.java

--- src/test/java/tn/esprit/devops/SarviceChambretImplTest.java
+++ src/test/java/tn/esprit/devops/SarviceChambretImplTest.java
@@ -1,5 +1,6 @@
 package tn.esprit.devops;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
@@ -17,7 +18,7 @@ import tn.esprit.devops.services.chambre.ChambreService;
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
-public class SarviceChambretImplTest {
+public class ChambreServiceImplTest {
     @Autowired
     private ChambreService chambreService;
     @Test
@@ -26,9 +27,10 @@ public class SarviceChambretImplTest {
         Chambre chambre = Chambre.builder()
                 .numeroChambre(10)
                 .typeC(TypeChambre.SIMPLE)
+
                 .build();
 
-        // Ajout de l'chambre 
+        // Ajout de l'chambre
         Chambre savedChambre = chambreService.addOrUpdate(chambre);
 
         // Vérifier que l'chambre a bien été ajouté et qu'il a un ID attribué
@@ -36,6 +38,8 @@ public class SarviceChambretImplTest {
 
         // Vérifier que le NumeroChambre ne dépassent pas 999
         Assertions.assertTrue(savedChambre.getNumeroChambre() < 999);
+
+
 
         // Nettoyer la base en supprimant l’chambre ajouté
         chambreService.delete(savedChambre);
