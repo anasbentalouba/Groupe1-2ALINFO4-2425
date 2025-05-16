cae0177c0877cc5a60eb6ea3ed9976fa19c0263e
Anas Ben Talouba <99347338+anasbentalouba@users.noreply.github.com>
2025-05-16 15:19:35 +0100

    Update ChambreServiceImplTest.java

--- src/test/java/tn/esprit/devops/ChambreServiceImplTest.java
+++ src/test/java/tn/esprit/devops/ChambreServiceImplTest.java
@@ -1,6 +1,6 @@
 package tn.esprit.devops;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
